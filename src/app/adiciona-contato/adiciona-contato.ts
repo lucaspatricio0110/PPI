5c94ae8e91361af741ab5e6ba989970ddc6cc9db
lucaspatricio0110 <lucas.patricio0110@gmail.com>
2025-07-23 23:05:44 -0300

    Update adiciona-contato.ts

--- src/app/adiciona-contato/adiciona-contato.ts
+++ src/app/adiciona-contato/adiciona-contato.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Contato, TipoContato } from './contato';
+import { Contato } from './contato';
 import { CommonModule } from '@angular/common';
 
 @Component({
