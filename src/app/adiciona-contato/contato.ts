bb855650ee89cba9d9a3a73824203aa730b90ee6
lucaspatricio0110 <lucas.patricio0110@gmail.com>
2025-07-23 22:36:06 -0300

    Update contato.ts

--- src/app/adiciona-contato/contato.ts
+++ src/app/adiciona-contato/contato.ts
@@ -3,11 +3,15 @@ export enum TipoContato {
 }
 export class Contato {
     nome: string
+    telefone: number
+    email: string
     aniversario: Date
     tipo: TipoContato
 
-    constructor(nm: string, an: Date, tipo: TipoContato) {
+    constructor(nm: string, tel: number, em: string, an: Date, tipo: TipoContato) {
         this.nome = nm
+        this.telefante = tel
+        this.eMail = em
         this.aniversario = an
         this.tipo = tipo
     }
