5c204442dcb92da901b71e754874844b2f3e6e41
lucaspatricio0110 <lucas.patricio0110@gmail.com>
2025-10-20 17:04:44 -0300

    Commit 16

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BuscaFilme } from './busca/busca-filme';
+import { BuscaLista } from './busca/busca-lista';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'filme', pathMatch: 'full' },
+  { path: 'filme', component: BuscaFilme },
+  { path: 'lista', component: BuscaLista },
+];
