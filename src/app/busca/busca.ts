527e1e98bc5b36348bd93254f4b9064d64851e88
lucaspatricio0110 <lucas.patricio0110@gmail.com>
2025-10-20 16:59:44 -0300

    Commit 13

--- src/app/busca/busca.ts
+++ src/app/busca/busca.ts
@@ -1,22 +1,44 @@
-import { Component, inject } from "@angular/core"
-import { FilmesService } from "../model/filmes-service"
+import { Component, inject } from "@angular/core";
+import { FilmesService } from "../model/filmes-service";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { Filme } from "../model/filme";
 
 @Component({
-    selector: 'app-busca',
-    imports: [],
-    templateUrl: './busca.html',
-    styleUrl: './busca.css'
+  selector: 'app-busca',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './busca.html',
+  styleUrls: ['./busca.css']
 })
 export class Busca {
-    private fs = inject(FilmesService)
+  private fs = inject(FilmesService);
+  titulo: string = '';
+  tipoBusca: string = 't'; // 't' = título único, 's' = lista
+  filme?: Filme;
+  lista: any[] = [];
+  erro: string = '';
 
-    constructor() {
-        this.buscarPorTitulo('The Godfather')
+  buscar() {
+    this.erro = '';
+    this.filme = undefined;
+    this.lista = [];
+
+    if (!this.titulo.trim()) {
+      this.erro = 'Digite um título para buscar.';
+      return;
     }
 
-    buscarPorTitulo(t: string) {
-        this.fs.obterFilme(t).subscribe(filme => {
-            console.log(filme)
-        })
+    if (this.tipoBusca === 't') {
+      this.fs.obterFilme(this.titulo).subscribe({
+        next: (res) => this.filme = res,
+        error: (err) => this.erro = 'Erro na busca: ' + err.message
+      });
+    } else {
+      this.fs.buscarLista(this.titulo).subscribe({
+        next: (res) => this.lista = res.Search || [],
+        error: (err) => this.erro = 'Erro na busca: ' + err.message
+      });
     }
+  }
 }
